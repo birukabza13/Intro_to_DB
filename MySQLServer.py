f8b5ea6ee52139ae9c49054a53d83a6b2c45c616
biruk geremew <birukabza13@gmail.com>
2024-10-14 19:41:22 +0300

    UPDated the file MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -2,14 +2,14 @@ import mysql.connector
 
 try:
     connection = mysql.connector.connect(host="localhost", user="root", password="pass")
-    create_db_query = "CREATE DATABASE alx_book_store"
+    create_db_query = "CREATE DATABASE IF NOT EXISTS alx_book_store"
     my_cursor = connection.cursor()
     my_cursor.execute(create_db_query)
     print("Database 'alx_book_store' created successfully!")
 
 except mysql.connector.Error as e:
     print("Error occured: " + e)
-    
+
 finally:
     my_cursor.close()
     connection.close()
