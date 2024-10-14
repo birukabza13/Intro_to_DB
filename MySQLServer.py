e2efc3f914876ef588c77fda4146379a45518b82
biruk geremew <birukabza13@gmail.com>
2024-10-14 19:40:25 +0300

    created the file MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,10 +1,15 @@
-from mysql.connector import connect, Error
+import mysql.connector
 
 try:
-    with connect(host="localhost", user="root", password="pass") as connection:
-        create_db_query = "CREATE DATABASE alx_book_store"
-        with connection.cursor() as cursor:
-            cursor.execute(create_db_query)
-            print("Database 'alx_book_store' created successfully!")
-except Error as e:
-    print(e)
+    connection = mysql.connector.connect(host="localhost", user="root", password="pass")
+    create_db_query = "CREATE DATABASE alx_book_store"
+    my_cursor = connection.cursor()
+    my_cursor.execute(create_db_query)
+    print("Database 'alx_book_store' created successfully!")
+
+except mysql.connector.Error as e:
+    print("Error occured: " + e)
+    
+finally:
+    my_cursor.close()
+    connection.close()
