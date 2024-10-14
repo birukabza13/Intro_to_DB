ac233d070989dcee7025829271c327ef05c9eff5
biruk geremew <birukabza13@gmail.com>
2024-10-14 19:20:53 +0300

    updated the file alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -37,5 +37,5 @@ CREATE TABLE
         book_id INT,
         quantity DOUBLE,
         FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-        FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+        FOREIGN KEY (book_id) REFERENCES Books(book_id)
     );
